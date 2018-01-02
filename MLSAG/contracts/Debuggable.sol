494527493f509edd4e59f58348ef45e0da0ba5f5
solidblu1992 <alegreve1992@gmail.com>
2018-01-01 19:26:20 -0600

    Create Debuggable.sol

---
+++ MLSAG/contracts/Debuggable.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.19;
+
+contract Debuggable {
+    //Debug Code
+    address public owner;
+    
+    function Debuggable() public {
+        owner = msg.sender;
+    }
+    
+	function Kill() public {
+    	if ( (msg.sender != owner) && (owner != 0) ) revert();
+
+    	selfdestruct(msg.sender);
+	}
+}

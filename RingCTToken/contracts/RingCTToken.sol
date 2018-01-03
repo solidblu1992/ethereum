f33eda831d44f700906e5fbcc0b0407e5690463f
solidblu1992 <alegreve1992@gmail.com>
2018-01-03 16:13:16 -0600

    Merge in Stealth Addresses to Ring CT Token

--- RingCTToken/contracts/RingCTToken.sol
+++ RingCTToken/contracts/RingCTToken.sol
@@ -14,9 +14,12 @@ contract RingCTToken is MLSAG_Verify {
 	uint256 public pub_key_count;
 	
 	//Stealth Address Mappings
-	mapping (uint256 => uint256) public dhe_points;
-	uint256 public dhe_point_count;
-	
+	mapping (address => uint256) public stx_pubviewkeys;    //Stores A=aG (public view key)
+    mapping (address => uint256) public stx_pubspendkeys;   //Stores B=bG (public spend key)
+    mapping (uint256 => uint256) public stx_dhe_points;     //Stores R=rG for each stealth transaction
+    mapping (uint256 => bool) public stx_dhepoints_reverse; //Reverse lookup for dhe_points
+	uint256 public stx_dhe_point_count;
+    
 	//Storage array of commitments which have been proven to be positive
 	mapping (uint256 => bool) public balance_positive;
 	
@@ -27,10 +30,86 @@ contract RingCTToken is MLSAG_Verify {
         //Constructor Code
     }
     
+    //Stealth Address Functions
+    //For a given msg.sender (ETH address) publish EC points for public spend and view keys
+    //These EC points will be used to generate stealth addresses
+    function PublishSTxPublicKeys(uint256 stx_pubspendkey, uint256 stx_pubviewkey)
+        public returns (bool success)
+    {
+        stx_pubspendkeys[msg.sender] = stx_pubspendkey;
+        stx_pubviewkeys[msg.sender] = stx_pubviewkey;
+        success = true;
+    }
+    
+    //Generate stealth transaction (off-chain)
+    function GenerateStealthTx(address stealth_address, uint256 random)
+        public constant returns (address dest, uint256 dhe_point)
+    {
+        //Verify that destination address has published spend and view keys
+        if (stx_pubspendkeys[stealth_address] == 0 || stx_pubviewkeys[stealth_address] == 0) return (0,0);
+        
+        //Generate DHE Point (R = rG)
+        uint256[2] memory temp;
+        
+        temp = ecMul(G1, random);
+        dhe_point = CompressPoint(temp);
+        
+        //Generate shared secret ss = H(rA) = H(arG)
+        temp[0] = HashOfPoint(ecMul(ExpandPoint(stx_pubviewkeys[stealth_address]), random));
+        
+        //Calculate target address public key P = ss*G + B
+        temp = ecMul(G1, temp[0]);
+        temp = ecAdd(temp, ExpandPoint(stx_pubspendkeys[stealth_address]));
+        
+        //Calculate target address from public key
+        dest = GetAddress(temp);
+    }
+    
+    //Calulates Stealth Address from index i of stx_dhepoints (off-chain)
+    //This function can be used to check for non-zero value addresses (are they applicable?)
+    function GetStealthTxAddress(uint256 i, uint256 stx_privviewkey, uint256 stx_pubspendkey)
+        public constant returns (address dest)
+    {
+        //If i >= stx_dhepoint_count then automatically the address is not used
+        if (i >= stx_dhe_point_count) return 0;
+        
+        //Expand dhe point (R = rG)
+        uint256[2] memory temp;
+        temp = ExpandPoint(stx_dhe_points[i]);
+        
+        //Calculate shared secret ss = H(aR) = H(arG)
+        temp[0] = HashOfPoint(ecMul(temp, stx_privviewkey));
+        
+        //Calculate target address public key P = ss*G + B
+        temp = ecMul(G1, temp[0]);
+        temp = ecAdd(temp, ExpandPoint(stx_pubspendkey));
+        
+        //Calculate target address from public key
+        dest = GetAddress(temp);
+    }
+    
+    //Calculates private key for stealth tx
+    function GetStealthTxPrivKey(uint256 i,uint256 stx_privviewkey, uint256 stx_privspendkey)
+        public constant returns (uint256 privkey)
+    {
+        //If i >= stx_dhepoint_count then automatically the address is not used
+        if (i >= stx_dhe_point_count) return 0;
+        
+        //Expand dhe point (R = rG)
+        uint256[2] memory temp;
+        temp = ExpandPoint(stx_dhe_points[i]);
+        
+        //Calculate shared secret ss = H(aR) = H(arG)
+        temp[0] = HashOfPoint(ecMul(temp, stx_privviewkey));
+        
+        //Calculate private key = ss + b
+        privkey = addmod(temp[0], stx_privspendkey, NCurve);
+    }
+    
     //Transaction Functions
-	//Deposit Ether as CT tokens to the specified ETH address
+	//Deposit Ether as CT tokens to the specified alt_bn_128 public key
 	//NOTE: this deposited amount will NOT be confidential, initial blinding factor = 0
-	function Deposit(uint256 dest_pub_key)
+	function Deposit(uint256 dest_pub_key, uint256 dhe_point)
     	payable public
 	{
     	//Incoming Value must be non-zero
@@ -41,6 +120,13 @@ contract RingCTToken is MLSAG_Verify {
     	
     	//Generate pedersen commitment and add to existing balance
     	token_committed_balance[dest_pub_key] = CompressPoint(ecMul(H, msg.value));
+    	pub_keys_by_index[pub_key_count] = dest_pub_key;
+    	pub_key_count++;
+    	
+    	//Store DHE point 
+    	stx_dhe_points[stx_dhe_point_count] = dhe_point;
+    	stx_dhepoints_reverse[dhe_point] = true;
+    	stx_dhe_point_count++;
     	
     	//Update global token supply
     	totalSupply += msg.value;
@@ -221,8 +307,14 @@ contract RingCTToken is MLSAG_Verify {
 			pub_keys_by_index[pub_key_count] = v.point1[0];		//Store public key
 			pub_key_count++;
 			
-			dhe_points[dhe_point_count] = v.point3[0];			//Store DHE point (for calculating stealth address)
-			dhe_point_count++;
+			//Publish DHE point if not already in published set
+			if (!stx_dhepoints_reverse[v.point3[0]]) {
+    			stx_dhe_points[stx_dhe_point_count] = v.point3[0];			//Store DHE point (for calculating stealth address)
+    			stx_dhepoints_reverse[v.point3[0]] = true;
+    			stx_dhe_point_count++;
+    			
+    			stx_dhepoints_reverse[v.point3[0]] = true;
+			}
 		}
 		
 		return true;
@@ -363,8 +455,14 @@ contract RingCTToken is MLSAG_Verify {
 			pub_keys_by_index[pub_key_count] = v.point1[0];		//Store public key
 			pub_key_count++;
 			
-			dhe_points[dhe_point_count] = v.point3[0];			//Store DHE point (for calculating stealth address)
-			dhe_point_count++;
+			//Publish DHE point if not already in published set
+			if (!stx_dhepoints_reverse[v.point3[0]]) {
+    			stx_dhe_points[stx_dhe_point_count] = v.point3[0];			//Store DHE point (for calculating stealth address)
+    			stx_dhepoints_reverse[v.point3[0]] = true;
+    			stx_dhe_point_count++;
+    			
+    			stx_dhepoints_reverse[v.point3[0]] = true;
+			}
 		}
 		
 		//Send redeemed value

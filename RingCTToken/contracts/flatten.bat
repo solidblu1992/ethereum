d890a5d2957303a730c0981f535f9839aec5bc97
solidblu1992 <alegreve1992@gmail.com>
2018-05-24 10:39:00 -0500

    Commit 05/24/2018-2
    Split RingCTToken off into its own repo.

--- RingCTToken/contracts/flatten.bat
+++
@@ -1,3 +0,0 @@
-py core.py --output RingCTTxVerify_Flattened.sol RingCTTxVerify.sol
-py core.py --output BulletProofVerify_Flattened.sol BulletProofVerify.sol
-py core.py --output RingCTToken_Flattened.sol RingCTToken.sol

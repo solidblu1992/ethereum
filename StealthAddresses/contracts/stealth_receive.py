aeaff3a996a83d0541d2b0b7d69f9d6ef3837ce3
solidblu1992 <alegreve1992@gmail.com>
2019-06-22 15:36:20 -0500

    Add files via upload

---
+++ StealthAddresses/contracts/stealth_receive.py
@@ -0,0 +1,65 @@
+import json
+from getpass import getpass
+from py_ecc import secp256k1
+from eth_keyfile import load_keyfile, decode_keyfile_json, create_keyfile_json
+from stealth_util import *
+
+#Import stealth scan and spend keys from scan_key.json and spend_key.json (using same password)
+#Read candidate stealth tx's from input.json.
+#If they belong to this stealth address, create keystore files for these tx's with same password
+def stealth_receive(directory):
+    #Import Scan and Spend Key
+    password = getpass()
+
+    scan_key = decode_keyfile_json(load_keyfile(directory + 'scan_key.json'), bytes(password, 'utf'))
+    spend_key = decode_keyfile_json(load_keyfile(directory + 'spend_key.json'), bytes(password, 'utf'))
+
+    pub_scan_key = secp256k1.privtopub(scan_key)
+    pub_spend_key = secp256k1.privtopub(spend_key)
+    stealth_address = GetStealthAddressFromKeys(pub_scan_key, pub_spend_key)
+
+    print("Stealth Address Imported: " + hex(int.from_bytes(stealth_address, 'big')))
+
+    #Import Inputs File
+    input_json = json.load(open(directory + "input.json"))
+    print()
+    print("Input Count: " + str(len(input_json["keys"])))
+
+    for key in input_json["keys"]:
+        #Extract and pad address and key fields
+        addr = key["address"][2:]
+        while len(addr) < 40:
+            addr = "0" + addr
+        addr = bytes.fromhex(addr)
+        
+        key = key["key"][2:]
+        while len(key) < 66:
+            key = "0" + key
+        key = bytes.fromhex(key)
+
+        print("Testing " + hex(int.from_bytes(addr, 'big')) + " ... ", end="")
+        point = ExpandPoint(key)
+
+        ss = GetSharedSecret(point, scan_key)
+        addr_exp = GetAddrFromSharedSecret(ss, pub_spend_key)
+
+        if addr == addr_exp:
+            print("HIT!")
+            priv_key = GetPrivKeyFromSharedSecret(ss, spend_key)
+
+            filename = "Keystore--" + hex(int.from_bytes(addr_exp, 'big'))[2:] + ".json"
+            
+            print("Creating " + filename + " ... ", end="")
+            filename = directory + filename
+            
+            with open(filename, mode='w') as file:
+                js = create_keyfile_json(int.to_bytes(priv_key, 32, 'big'), bytes(password, 'utf'))
+                json.dump(js, file)
+
+            print("COMPLETE!")
+        else:
+            print("MISS!")
+
+if __name__ == "__main__":
+    directory = "C:/"
+    stealth_receive(directory)
---
+++ StealthAddresses/contracts/stealth_send.py
@@ -0,0 +1,41 @@
+from stealth_util import *
+
+def stealth_send(stealth_address, count=3):
+    if type(stealth_address) == int:
+        stealth_address = int.to_bytes(stealth_address, 65, 'big')
+        
+    pub_scan_key, pub_spend_key = GetKeysFromStealthAddress(stealth_address)
+
+    out = "{\n"
+    out += "\t" + "\"keys\":[\n"
+
+    tabs = 2
+    for i in range(0, count):
+        R, addr = CreateStealthTx(pub_scan_key, pub_spend_key)
+        out += "\t"*tabs + "{\n"
+
+        tabs += 1
+        out += "\t"*tabs + "\"key\":\"" + R + "\",\n"
+        out += "\t"*tabs + "\"addr\":\"" + addr + "\"\n"
+        tabs -= 1
+        
+        out += "\t"*tabs + "}"
+
+        if i < (count-1):
+            out += ","
+
+        out += "\n"
+        
+    tabs -= 1
+    out += "\t"*tabs + "]\n"
+    
+    out += "}"
+
+    return out
+
+if __name__ == "__main__":
+    stealth_address = 0x1675ac9304b7cfe986d286ebb910ba217f59ed5bb36deb2fa643c6b7847252d638b3b63ae513fcb9fc28a01e9d024cbe4a1fa4571bf577d28c29e32d4bd9ebbac
+    out = stealth_send(stealth_address)
+
+    print("New tx addresses " + hex(stealth_address))
+    print(out)

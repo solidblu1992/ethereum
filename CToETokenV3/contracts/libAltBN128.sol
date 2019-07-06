4cf07d381b439b676afc37f61edb2aa58ae4788c
solidblu1992 <alegreve1992@gmail.com>
2019-07-06 14:28:23 -0500

    Split 1-bit range proof off into new folder

---
+++ CToETokenV3/contracts/libAltBN128.sol
@@ -0,0 +1,111 @@
+pragma solidity ^0.5.9;
+
+library AltBN128 {
+    //Scalar Functions
+	function GetN() internal pure returns (uint) {
+		return 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001;
+	}
+	
+	function GetP() internal pure returns (uint) {
+		return 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47;
+	}
+	
+	function GetCompressedPointSignFlag() internal pure returns (uint) {
+	    return 0x8000000000000000000000000000000000000000000000000000000000000000;
+	}
+	
+	function NegateFQ(uint fq) internal pure returns (uint) {
+		return GetP() - (fq % GetP());
+	}
+	
+	function NegateScalar(uint s) internal pure returns (uint) {
+		return GetN() - (s % GetN());
+	}
+	
+	//Point Functions
+	function GetG1() internal pure returns (uint G1x, uint G1y) {
+	    return (1, 2);
+	}
+	
+	function CompressPoint(uint Px, uint Py) internal pure returns (uint x_compressed) {
+	    x_compressed = Px;
+        
+        if (Py & 1 != 0) {
+            x_compressed |= GetCompressedPointSignFlag();
+        }
+	}
+	
+	function ExpandPoint(uint x_compressed) internal view returns (uint Px, uint Py) {
+        //Check bit flag
+        bool odd = (x_compressed & GetCompressedPointSignFlag() != 0);
+        
+        //Remove bit flag
+        if (odd) {
+            x_compressed &= ~GetCompressedPointSignFlag();
+        }
+        
+        //Get y-coord
+        (Px, Py) = G1PointFromX(x_compressed);
+        
+        //Check sign, correct if necessary
+        if (odd) {
+            if (Py & 1 == 0) {
+                Py = NegateFQ(Py);
+            }
+        }
+        else {
+            if (Py & 1 == 1) {
+                Py = NegateFQ(Py);
+            }
+        }
+    }
+	
+	//Check to see if G1Point is on curve
+	function IsOnCurve(uint Px, uint Py) internal pure returns (bool) {
+	    //(y^2 == x^3 + 3) % p
+	    uint p = GetP();
+	    uint left = mulmod(Py, Py, p);
+	    uint right = addmod(mulmod(mulmod(Px, Px, p), Px, p), 3, p);
+	    
+	    return (left == right);
+	}	
+	
+    //Get G1Point from desired x coordinate (increment x if not on curve)
+	function G1PointFromX(uint x) internal view returns (uint Px, uint Py) {
+	    uint p = GetP();
+	    x = x % p;
+	    
+	    uint[] memory data = new uint[](6);
+	    data[0] = 0x20;
+	    data[1] = 0x20;
+	    data[2] = 0x20;
+	    //data[3] = 0;
+	    data[4] = 0xc19139cb84c680a6e14116da060561765e05aa45a1c72a34f082305b61f3f52;  // (p+1)/4
+	    data[5] = p;
+	    
+        bool onCurve = false;
+        while(!onCurve) {
+            //Get y coordinate
+    	    data[3] = addmod(mulmod(mulmod(x, x, p), x, p), 3, p);
+    	    
+    	    assembly {
+    	        //Call Big Int Mod Exp: (y_squared)^a % p, store y in data[3]
+        	    let success := staticcall(sub(gas, 2000), 0x05, add(data, 0x20), 0xC0, add(data, 0x80), 0x20)
+    	    }
+    	    
+    	    //Check y coordinate
+    	    onCurve = IsOnCurve(x, data[3]);
+    	    if (!onCurve) {
+    	        x = addmod(x, 1, p);
+    	    }
+        }
+        
+        (Px, Py) = (x, data[3]);
+	}
+	
+	//Get G1Point from input address
+	function G1PointFromAddress(address addr) internal view returns (uint Px, uint Py) {
+	    uint x = uint(keccak256(abi.encodePacked(addr)));
+	    return G1PointFromX(x);
+	}
+}
--- CToETokenV3/contracts/libOneBitRangeProof.sol
+++ CToETokenV3/contracts/libOneBitRangeProof.sol
@@ -1,23 +1,83 @@
 pragma solidity ^0.5.9;
 
-library OneBitRangeProof {
-	///Curve parameters and generators
-	uint constant private N = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001;
-	uint constant private P = 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47;
+import "./libAltBN128.sol";
+
+library OneBitRangeProof {	
+	struct Data {
+		uint Cx;
+		uint Cy;
+		uint c0;
+		uint s0;
+		uint s1;
+	}
 	
-    function Verify(uint Cx, uint Cy, uint Hx, uint Hy, uint c0, uint s0, uint s1) internal view returns (bool) {
+    //Low Level Functions
+    function CalcNegAssetH(address asset_address) internal view returns (uint Hx, uint Hy_neg) {
+        (Hx, Hy_neg) = AltBN128.G1PointFromAddress(asset_address);
+        Hy_neg = AltBN128.NegateFQ(Hy_neg);
+    }	
+	
+	//High Level Functions
+	function FromBytes(bytes memory b) internal view returns (Data[] memory proof) {
+		//b must be a multiple of 32*4=128 or 32*5=160 bytes long
+		bool compressed_proof = false;
+		
+		if (b.length % 128 == 0) {
+		    compressed_proof = true;
+		    proof = new Data[](b.length / 128);
+		}
+		else {
+		    require(b.length % 160 == 0);
+		    proof = new Data[](b.length / 160);
+		}
+		
+		//Load bytes 32 at a time, shift off unused bits
+		uint buffer;
+		uint offset = 32; //1st byte is length, unneeded
+		
+		//Extract Proofs
+		for (uint i = 0; i < proof.length; i++) {
+		    if (compressed_proof) {
+        		assembly { buffer := mload(add(b, offset)) }
+        	    (proof[i].Cx, proof[i].Cy) = AltBN128.ExpandPoint(buffer);
+        	    offset += 32;
+		    }
+		    else {
+		        assembly { buffer := mload(add(b, offset)) }
+        	    proof[i].Cx = buffer;
+        	    offset += 32;
+        	    
+        	    assembly { buffer := mload(add(b, offset)) }
+        	    proof[i].Cy = buffer;
+        	    offset += 32;
+		    }
+    	    
+    		assembly { buffer := mload(add(b, offset)) }
+    	    proof[i].c0 = buffer;
+    	    offset += 32;
+    		
+    		assembly { buffer := mload(add(b, offset)) }
+    	    proof[i].s0 = buffer;
+    	    offset += 32;
+    		
+    		assembly { buffer := mload(add(b, offset)) }
+    	    proof[i].s1 = buffer;
+    	    offset += 32;
+		}
+	}
+	
+    function Verify(Data memory proof, uint Hx, uint Hy_neg) internal view returns (bool) {
         //Allocate memory
         uint[] memory data = new uint[](7);
-        
-        
+
         //Do first ring segment
         //2 multiplications, 1 addition, and 1 keccak256
-        data[0] = Cx;
-        data[1] = Cy;
-        data[2] = c0;
+        data[0] = proof.Cx;
+        data[1] = proof.Cy;
+        data[2] = proof.c0;
         data[3] = 1;    //G1x
         data[4] = 2;    //G1y
-        data[5] = s0;
+        data[5] = proof.s0;
         //data[6] = 0;  //Unused
         
         bool success;
@@ -42,10 +102,10 @@ library OneBitRangeProof {
         //data[0] = 0;  //Unused
         //data[1] = 0;  //Unused
         //data[2] = c1; //Unused
-        data[3] = Cx;
-        data[4] = Cy;
+        data[3] = proof.Cx;
+        data[4] = proof.Cy;
         data[5] = Hx;
-        data[6] = P - (Hy % P); //negate H
+        data[6] = Hy_neg;
         
         assembly {
             //Add the two points in data[3, 4, 5, 6], store result in data[0, 1]
@@ -60,7 +120,7 @@ library OneBitRangeProof {
         //data[2] = c1;
         data[3] = 1;    //G1x
         data[4] = 2;    //G1y
-        data[5] = s1;
+        data[5] = proof.s1;
         //data[6] = 0;  //Unused
         
         assembly {
@@ -81,27 +141,6 @@ library OneBitRangeProof {
          }
          
          //Check for ring continuity
-         return(data[2] == c0);
-    }
-}
-
-contract TestOneBitRangeProof {
-    address private debugOwner;
-    
-    constructor() public {
-        debugOwner = msg.sender;
-    }
-    
-    function DebugKill() public {
-        require(msg.sender == debugOwner);
-        selfdestruct(msg.sender);
-    }
-    
-    function Test(uint Cx, uint Cy, uint Hx, uint Hy, uint c0, uint s0, uint s1) public view returns (bool) {
-        return OneBitRangeProof.Verify(Cx, Cy, Hx, Hy, c0, s0, s1);
-    }
-    
-    function GasTest(uint Cx, uint Cy, uint Hx, uint Hy, uint c0, uint s0, uint s1) public returns (bool) {
-        return OneBitRangeProof.Verify(Cx, Cy, Hx, Hy, c0, s0, s1);
+         return(data[2] == proof.c0);
     }
 }

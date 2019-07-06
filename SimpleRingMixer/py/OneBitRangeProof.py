ac025698e5cec7a06eb08a5db6c7d62da387a2d7
solidblu1992 <alegreve1992@gmail.com>
2019-07-06 12:30:07 -0500

    Update OneBitRangeProof.py

--- SimpleRingMixer/py/OneBitRangeProof.py
+++ SimpleRingMixer/py/OneBitRangeProof.py
@@ -4,12 +4,33 @@ from random import SystemRandom
 
 #"Private Keys"
 sr = SystemRandom()
+asset_address = 0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359 #DAI
 v = 1
 bf = sr.getrandbits(256)
 
 G1 = bn128.G1
-H = (   bn128.FQ(0x2854ddec56b97fb3a6d501b8a6ff07891ce7aeb22c1cc74cf0a18ebc3f15220b),
-        bn128.FQ(0x23a967b0d240d4264fea929d6a02ba4b7c612c0a4ef611e92eb011aa854cdbf7)    )
+
+#Get asset H
+def H_from_address(address):
+    x = bn128.FQ(int.from_bytes(keccak_256(address.to_bytes(20,'big')).digest(), 'big'))
+
+    on_curve = False
+    while not on_curve:
+        y2 = x**3 + bn128.b
+        y = y2**((bn128.field_modulus+1)//4)
+
+        if (y**2 == y2):
+            on_curve = True
+        else:
+            x += 1
+
+    return (x, y)
+
+if asset_address == 0:
+    H = (   bn128.FQ(0x2854ddec56b97fb3a6d501b8a6ff07891ce7aeb22c1cc74cf0a18ebc3f15220b),
+            bn128.FQ(0x23a967b0d240d4264fea929d6a02ba4b7c612c0a4ef611e92eb011aa854cdbf7)    )
+else:
+    H = H_from_address(asset_address)
 
 #Calculate Ring
 C = bn128.multiply(G1, bf)

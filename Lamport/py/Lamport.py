98cc6ab07da55df4de2765200ef725084d9ac144
solidblu1992 <alegreve1992@gmail.com>
2019-07-14 13:31:29 -0500

    Update Lamport.py

--- Lamport/py/Lamport.py
+++ Lamport/py/Lamport.py
@@ -1,57 +1,24 @@
 def getRandom(count=1):
-    import random
+    from random import SystemRandom
+    sr = SystemRandom()
 
     if (count == 1):
-        out = random.SystemRandom().getrandbits(255)
+        out = sr.getrandbits(256)
     else:
         out = []
         for i in range(0, count):
-            out = out + [random.SystemRandom().getrandbits(255)]
+            out = out + [sr.getrandbits(256)]
 
     return out
-
-def int_to_iterable(i):
-    x = []
-    bits = 0
-    while i > 0:
-        y = i & (0xFF << bits)
-        x = [(y >> bits)] + x
-        i = i - y
-        bits = bits + 8
-
-    return x
-
-def int_to_bytes32(i):
-    x = bytes(int_to_iterable(i% 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff))
-
-    if (len(x) < 32):
-        y = bytes(32 - len(x))
-        x = y+x
-
-    return x
-
-def bytes_to_int(bytes):
-    result = 0
-
-    for b in bytes:
-        result = result * 256 + int(b)
-
-    return result
 
 class Lamport:
     PrivateKey = 0
     PublicKey = 0
     
     def __init__(self):
-        import sha3
+        from sha3 import keccak_256
         self.PrivateKey = getRandom(512)
-        self.PublicKey = [0]*512
-
-        for i in range(0, 512):
-            self.PublicKey[i] = bytes_to_int(sha3.keccak_256(int_to_bytes32(self.PrivateKey[i])).digest())
+        self.PublicKey = [keccak_256(bytes.fromint(self.PrivateKey[i], 256, 'big')).digest() for i in range(0, 512)]
 
     def Sign(self):
         print()
-
-
-            

2d83ac8e2e6c49629417da1ef923de625eed7dcd
solidblu1992 <alegreve1992@gmail.com>
2023-05-20 13:45:20 -0500

    Update index.js

--- StealthAddresses/js/index.js
+++ StealthAddresses/js/index.js
@@ -2,6 +2,11 @@ const curve = require('noble-secp256k1');
 const bufToBigInt = require('bigint-conversion').bufToBigint;
 const { keccak256 } = require("ethereum-cryptography/keccak.js");
 
+//Helper Function
+function eth_addr_from_pubkey(Y) {
+    return keccak256(Y.toRawBytes(false)).buffer.slice(0,20);
+}
+
 //Main Code Block
 class StealthTx {
     constructor (R = null, addr = null, x = null) {
@@ -47,13 +52,18 @@ class StealthAddress {
         return new StealthAddress(Yv, Ys, null, null);
     }
 
+    //Create a Public version of Stealth Address Object (stripping out private keys)
+    asPublic() {
+        return new StealthAddress(this.Yv, this.Ys, null, null);
+    }
+
     async generateTx() {
         var r = bufToBigInt(curve.utils.randomPrivateKey());
         var R = curve.Point.BASE.multiply(r);
         var shared_secret = await curve.utils.sha256(this.Yv.multiply(r).toRawBytes(false));
         shared_secret = bufToBigInt(shared_secret);
         var Y = curve.Point.BASE.multiply(shared_secret).add(this.Ys);
-        var addr = keccak256(Y.toRawBytes(false)).buffer.slice(0,20);
+        var addr = eth_addr_from_pubkey(Y);
         return new StealthTx(R, addr);
     }
 
@@ -61,7 +71,7 @@ class StealthAddress {
         var shared_secret = await curve.utils.sha256(tx.R.multiply(this.xv).toRawBytes(false));
         shared_secret = bufToBigInt(shared_secret);
         var Y = curve.Point.BASE.multiply(shared_secret).add(this.Ys);
-        var addr = keccak256(Y.toRawBytes(false)).buffer.slice(0,20);
+        var addr = eth_addr_from_pubkey(Y);
         if (bufToBigInt(addr) != bufToBigInt(tx.addr)) { return false; }
         tx.x = (shared_secret + this.xs) % curve.CURVE.n;
         return true;
@@ -69,24 +79,40 @@ class StealthAddress {
 
     print() {
         console.log("Stealth Address: 0x" + this.Yv.toHex(true) + this.Ys.toHex(true));
+        if (this.xv == null) {
+            console.log("\tPrivate View Key: unknown");
+        }
+        else {
+            console.log("\tPrivate View: known");
+        }
+        if (this.xs == null) {
+            console.log("\tPrivate Spend Key: unknown");
+        }
+        else {
+            console.log("\tPrivate Spend: known");
+        }
     }
 }
-async function generate_stealth_address() {
+
+async function test_module() {
     //Generate Stealth Address
     var stealth_address = StealthAddress.fromPrivKeys();
     stealth_address.print();
 
     //Generate Stealth Tx
-    var tx = await stealth_address.generateTx();
+    var pub_stealth_address = stealth_address.asPublic();
+    pub_stealth_address.print();
+    var tx = await pub_stealth_address.generateTx();
     tx.print();
 
-    if (stealth_address.recoverTx(tx)) {
+    //Recover Tx Private Key using stealth address
+    var success = await stealth_address.recoverTx(tx);
+    if (success) {
         console.log("recoverTx success");
     }
     else {
         console.log("recoverTx failed or not for this stealth address");
     }
-    tx.print();
 }
 
-generate_stealth_address();
+test_module();
